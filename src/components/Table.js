1216c0c58bc8f04d1d966f2e021a1c39125b82c8
mwomack <michael.womack84@gmail.com>
2020-02-28 10:06:58 -0800

    Components created for Button, Table, and Search

---
+++ src/components/Table.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import Button from "./Button";
+
+const largeColumn = {
+  width: "40%"
+};
+
+const midColumn = {
+  width: "27%"
+};
+
+const smallColumn = {
+  width: "11%"
+};
+
+const Table = ({ list, onDismiss }) => (
+  <div className="table">
+    <div className="table-header">
+      <span style={largeColumn}>Article Title</span>
+      <span style={midColumn}>Author</span>
+      <span style={smallColumn}>Comments</span>
+      <span style={smallColumn}>Points</span>
+      <span style={smallColumn}></span>
+    </div>
+    {list.map(item => (
+      <div key={item.objectID} className="table-row">
+        <span style={largeColumn}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={midColumn}>{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>{item.points}</span>
+        <span style={smallColumn}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    ))}
+  </div>
+);
+
+export default Table;
