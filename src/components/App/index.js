79bfcb414cd28d66aeb408be39384bb743469798
mwomack <michael.womack84@gmail.com>
2020-03-16 13:49:10 -0700

    Restructer folder setup

---
+++ src/components/App/index.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import PropTypes from "prop-types";
+
+import Button from "../Button";
+import Table from "../Table";
+import Search from "../Search";
+import Header from "../Header";
+import Loading from "../Loading";
+
+import {
+  PATH_BASE,
+  PARAM_HITS_PER_PAGE,
+  PARAM_QUERY,
+  PARAM_PAGE
+} from "../../constants/apiParams.js";
+
+const App = () => {
+  const [results, setResults] = useState({});
+  const [searchTerm, setSearchTerm] = useState(PARAM_QUERY);
+  const [searchKey, setSearchKey] = useState(PARAM_QUERY);
+  const [error, setError] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  const needsToSearchTopStories = searchTerm => {
+    return !results[searchTerm];
+  };
+
+  const setSearchTopStories = result => {
+    const { hits, page } = result;
+
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
+
+    const updatedHits = [...oldHits, ...hits];
+
+    const newResults = { ...results, [searchKey]: { hits: updatedHits, page } };
+
+    setResults(newResults);
+    setLoading(false);
+  };
+
+  const fetchSearchTopStories = (searchTerm, page = 0) => {
+    setLoading(true);
+
+    axios
+      .get(PATH_BASE + searchTerm + PARAM_HITS_PER_PAGE + PARAM_PAGE + page)
+      .then(response => {
+        setSearchTopStories(response.data);
+        console.log("API call", response.data);
+      })
+      .catch(error => {
+        setError(error);
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    setSearchKey(searchTerm);
+
+    if (needsToSearchTopStories(searchTerm)) {
+      fetchSearchTopStories(searchTerm);
+    }
+  }, [searchKey]);
+
+  const onSearchChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const onSearchSubmit = event => {
+    setSearchKey(searchTerm);
+
+    event.preventDefault();
+  };
+
+  const onDismiss = id => {
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = results[searchKey].hits.filter(isNotId);
+    // same as ^^ // const updatedHits = results[searchKey].hits.filter(item => item.objectID !== id);
+    setResults({ ...results, [searchKey]: { hits: updatedHits } });
+  };
+
+  const page = (results && results[searchKey] && results[searchKey].page) || 0;
+  const list = (results && results[searchKey] && results[searchKey].hits) || [];
+
+  return (
+    <div className="page">
+      <div>
+        <Header
+          value={searchTerm}
+          onChange={onSearchChange}
+          onSubmit={onSearchSubmit}
+        >
+          Search
+        </Header>
+      </div>
+      {error ? (
+        <div className="interactions">
+          <p>
+            Something went wrong{" "}
+            <span role="img" aria-label="confused imoji">
+              🤔
+            </span>
+          </p>
+        </div>
+      ) : (
+        <Table list={list} onDismiss={onDismiss} />
+      )}
+      <div className="interactions">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <Button onClick={() => fetchSearchTopStories(searchKey, page + 1)}>
+            More
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired
+};
+
+Search.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired
+};
+
+export default App;
